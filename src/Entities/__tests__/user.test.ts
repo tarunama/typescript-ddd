d45cc5b8291b8215d0af81ef54006a75b0ff2696
tarunama <sakeharami@gmail.com>
2022-12-22 22:14:35 +0900

    chore(vitest): replace jest into vitest

--- src/Entities/__tests__/user.test.ts
+++ src/Entities/__tests__/user.test.ts
@@ -1,3 +1,5 @@
+import { test, expect } from "vitest"
+
 import { User, UserId } from "../user";
 
 test('equal method', () => {
--- src/Values/__tests__/money.test.ts
+++ src/Values/__tests__/money.test.ts
@@ -1,3 +1,5 @@
+import { test, expect } from "vitest"
+
 import { Money } from "../money";
 
 test('add method', () => {
