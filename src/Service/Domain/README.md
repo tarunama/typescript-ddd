c366cb56686fb7b909dc65db1467db5f85045948
tarunama <sakeharami@gmail.com>
2022-12-27 12:39:18 +0900

    feat: add domain service

---
+++ src/Service/Domain/README.md
@@ -0,0 +1,4 @@
+# Domain Service
+
+- Domain(Values, Entities)に定義すると不自然な振る舞いを定義する
+  - ex) ユーザーの重複確認など
---
+++ src/Service/Domain/UserService.ts
@@ -0,0 +1,9 @@
+import { User } from "@/Domain/Entities/user";
+
+export class UserService {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  exist(user: User): boolean {
+    // TODO: impl
+    return false;
+  }
+}
